5e6ca914f70c2009d746e3fee54dcb18074467d1
Aman Kumar Bhagat <bhagatamankumar1999@gmail.com>
2023-06-21 08:34:33 +0530

    initial commit

--- DSA_By_CipherSchools/merge-sorted-arrays-Q1.cpp
+++ DSA_By_CipherSchools/merge-sorted-arrays-Q1.cpp
@@ -1,36 +1,32 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// pass by value/pass by reference
-void mergeSortedArrays(vector<int> &a, int m, vector<int> &b, int n) {
-int i=m-1,j=n-1,k=m+n - 1;
-while (i >= 0 && j >= 0) {
-if (a[i] > b[j]) {
-    a[k] = a[i];
-    i--;
-} else {
-a[k] = b[j];
-j--;
-}
-k--;
-}
-while (j >= 0) {
-a[k] = b[j];
-j--;
-k--;
+void mergeSortedArrays(vector<int>&a, int m , vector<int>&b, int n){
+     int i = m - 1, j = n - 1, k = m + n - 1;
 
+        while (i >= 0 && j >= 0) {
+            if (a[i] > b[j]) {
+                a[k--] = a[i--];
+            } else {
+                a[k--] = b[j--];
+            }
+        }
+        while (j >= 0) {
+            a[k--] = b[j--];
+        }
 }
-}
-
-int main() {
-    vector<int> a = {11, 14, 19, 22, 26};
-    vector<int> b = {1, 4, 6, 9};
-
-    mergedSortedArrays(a, a.size(), b, b.size());
-    for (auto i : a)
-    {
-        cout << i << " " << endl;
+int main(){
+    
+    vector<int> a = {1, 2, 3, 0, 0, 0};
+    vector<int> b = {2, 5, 6};
+    int m = 3;
+    int n = 3;
+    
+    mergeSortedArrays(a,m,b,n);
+    
+    cout << "After merging: ";
+    for (int num : a) {
+        cout << num << " ";
     }
-
-
+    cout << endl;
 }
